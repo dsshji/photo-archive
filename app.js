dc5180e78b8e3a82a83cfaccf5a3bba924614144
Daria <dshxlva@yandex.ru>
2026-04-21 15:07:30 +0300

    initial setup of D3

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-// initialize a database of IndexedDB
-let db;
-openDB().then(result => {
-  db = result;
-});
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
+const width = 800;
+const height = 600;
 
+// test data
 const tree = new PhotoTree("Life");
 tree.addEra("Moscow/Childhood");
-tree.addPhoto("First day", "", "Moscow/Childhood");
-tree.saveTree();
+tree.addEra("Moscow/Now");
+tree.addEra("Abu Dhabi");
 
-const tree2 = new PhotoTree("Life");
-tree2.loadTree();
-console.log(tree2.root);
+// convert to D3 format
+const root = d3.hierarchy(tree.root);
+const treeLayout = d3.tree().size([width, height]);
+treeLayout(root);
+
+console.log(root);
